e57ebb1d61f4aedfe4c7ffb885dd2b14d8a3109d
Uday Kiran Reddy Saddipally <143345142+Udayreddy2608@users.noreply.github.com>
2023-11-16 14:19:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
-MY_EMAIL="saitesting26072001@gmail.com"
-MY_PASSWORD="usygesuqmfmyzifz"
+MY_EMAIL="YOUR EMAIL"
+MY_PASSWORD="YOUR APP PASSWORD"
 import smtplib
-URL="https://www.amazon.in/dp/B09KN1JX8H/?coliid=I5UBHFR62XV9J&colid=2HP1SX5ZKZQRI&psc=1&ref_=list_c_wl_lv_ov_lig_dp_it_im"
+URL="URL OF THE PRODUCT'S  Price YOU WANT TO TRACK "
 headers={
-    "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36",
-    "Accept-Language":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
+    "User-Agent":"YOUR USER AGENT",
+    "Accept-Language":"YOUR DEVICE ACCEPT LANGUAGE"
 }
 response=requests.get(url=URL,headers=headers)
 webpage=response.text
@@ -19,6 +19,6 @@ if price==price:
     connection=smtplib.SMTP("smtp.gmail.com")
     connection.starttls()
     connection.login(user=MY_EMAIL,password=MY_PASSWORD)
-    connection.sendmail(from_addr=MY_EMAIL,to_addrs="udayreddys2607@gmail.com",
+    connection.sendmail(from_addr=MY_EMAIL,to_addrs="YOUR EMAIL",
                         msg=f"subject:Price Drop Alert\n\nThe price of the product you are looking for"
                             f"is dropped down to {price}/- on Amazon\n\nClick to buy{URL}")
